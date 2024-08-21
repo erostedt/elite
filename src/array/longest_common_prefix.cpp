3b1f1fb0f31d94bae43fcaa49bb9370b511d8bdb
Eric Rostedt <rostedteric@gmail.com>
2024-08-21 20:40:46 +0200

    add longest_common_prefix implementation

--- src/array/longest_common_prefix.cpp
+++ src/array/longest_common_prefix.cpp
@@ -23,19 +23,41 @@ Constraints:
 strs[i] consists of only lowercase English letters.
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 #include "assert.hpp"
 
 using namespace std;
+
+size_t first_diff(std::string_view str, std::string_view other)
+{
+    size_t min = std::min(std::size(str), std::size(other));
+    for (size_t i = 0; i < min; ++i)
+    {
+        if (str[i] != other[i])
+        {
+            return i;
+        }
+    }
+    return min;
+}
 
 class Solution
 {
   public:
     string longestCommonPrefix(vector<string> &strs)
     {
-        NOT_IMPLEMENTED;
+        std::string_view prefix = strs.front();
+        size_t size = std::size(prefix);
+        for (size_t i = 1; i < std::size(strs); ++i)
+        {
+            size = first_diff(prefix, strs[i]);
+            prefix = prefix.substr(0, size);
+        }
+        return std::string(prefix);
     }
 };
 
