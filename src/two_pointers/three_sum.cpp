332cf7d5e671c63d3c4af38991577dd6a2e33310
Eric Rostedt <rostedteric@gmail.com>
2024-08-24 23:18:18 +0200

    add three sum implementation

--- src/two_pointers/three_sum.cpp
+++ src/two_pointers/three_sum.cpp
@@ -34,18 +34,97 @@ Constraints:
 -105 <= nums[i] <= 105
 */
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <unordered_set>
 #include <vector>
 
 #include "assert.hpp"
 
 using namespace std;
+
+struct Triplet
+{
+    int first;
+    int second;
+    int third;
+};
+
+struct TripletEqual
+{
+    bool operator()(const Triplet &lhs, const Triplet &rhs) const
+    {
+        return lhs.first == rhs.first && lhs.second == rhs.second && lhs.third == rhs.third;
+    }
+};
+
+struct TripletHash
+{
+    size_t operator()(const Triplet &triplet) const
+    {
+        std::size_t hash = 0;
+        std::hash<int> int_hasher{};
+        hash ^= int_hasher(triplet.first) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        hash ^= int_hasher(triplet.second) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        hash ^= int_hasher(triplet.third) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        return hash;
+    }
+};
+
+using TripletSet = std::unordered_set<Triplet, TripletHash, TripletEqual>;
+
+template <typename Iterator>
+void _two_sum(Iterator forward, Iterator backward, int target, std::vector<std::vector<int>> &triplets,
+              TripletSet &seen)
+{
+    while (forward < backward)
+    {
+        const int sum = *forward + *backward;
+        if (sum == target)
+        {
+            const Triplet triplet{-target, *forward, *backward};
+            if (!seen.contains(triplet))
+            {
+                triplets.push_back({-target, *forward, *backward});
+                seen.insert(triplet);
+            }
+            ++forward;
+        }
+        else if (sum > target)
+        {
+            --backward;
+        }
+        else
+        {
+            ++forward;
+        }
+    }
+}
+
 class Solution
 {
   public:
     vector<vector<int>> threeSum(vector<int> &nums)
     {
-        NOT_IMPLEMENTED;
+        if (std::size(nums) < 3)
+        {
+            return {};
+        }
+
+        std::sort(std::begin(nums), std::end(nums));
+
+        std::vector<std::vector<int>> triplets;
+        auto forward = std::cbegin(nums);
+        auto last = std::prev(std::cend(nums));
+        TripletSet seen;
+        while (forward < last)
+        {
+            const int target = -(*forward);
+            _two_sum(std::next(forward), last, target, triplets, seen);
+            ++forward;
+        }
+        return triplets;
     }
 };
 
@@ -63,13 +142,21 @@ int main()
     {
         std::vector numbers = {0, 1, 1};
 
-        const std::vector<std::vector<int>> expected_output = {{}};
+        const std::vector<std::vector<int>> expected_output = {};
         const std::vector<std::vector<int>> output = solution.threeSum(numbers);
 
         Assert::matrix_equals(output, expected_output);
     }
     {
         std::vector numbers = {0, 0, 0};
+
+        const std::vector<std::vector<int>> expected_output = {{0, 0, 0}};
+        const std::vector<std::vector<int>> output = solution.threeSum(numbers);
+
+        Assert::matrix_equals(output, expected_output);
+    }
+    {
+        std::vector numbers = {0, 0, 0, 0};
 
         const std::vector<std::vector<int>> expected_output = {{0, 0, 0}};
         const std::vector<std::vector<int>> output = solution.threeSum(numbers);
