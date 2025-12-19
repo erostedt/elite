1d45fe4e004276a53a38f37b3a189d0503b20199
Eric Rostedt <rostedteric@gmail.com>
2025-12-19 09:50:38 +0100

    longest substring without repeating characters

--- src/sliding_window/longest_substring_without_repeating_characters.cpp
+++ src/sliding_window/longest_substring_without_repeating_characters.cpp
@@ -10,6 +10,7 @@ Example 1:
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
+
 Example 2:
 
 Input: s = "bbbbb"
@@ -29,18 +30,39 @@ Constraints:
 s consists of English letters, digits, symbols and spaces.
 */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 #include "assert.hpp"
 
 using namespace std;
+
+void print_vec(vector<char> v)
+{
+    string s = {v.begin(), v.end()};
+    std::cout << s << std::endl;
+}
 class Solution
 {
   public:
     int lengthOfLongestSubstring(string s)
     {
-        NOT_IMPLEMENTED;
+        vector<char> substr{};
+        size_t longest = 0;
+        for (const auto c : s)
+        {
+            const auto it = find(begin(substr), end(substr), c);
+            if (it != end(substr))
+            {
+                longest = max(longest, substr.size());
+                substr.erase(begin(substr), next(it));
+            }
+
+            substr.push_back(c);
+        }
+        return max(longest, substr.size());
     }
 };
 
@@ -65,6 +87,24 @@ int main()
     }
     {
         const std::string s = "pwwkew";
+
+        const int expected_output = 3;
+        const int output = solution.lengthOfLongestSubstring(s);
+
+        Assert::equal(output, expected_output);
+    }
+
+    {
+        const std::string s = " ";
+
+        const int expected_output = 1;
+        const int output = solution.lengthOfLongestSubstring(s);
+
+        Assert::equal(output, expected_output);
+    }
+
+    {
+        const std::string s = "aabaab!bb";
 
         const int expected_output = 3;
         const int output = solution.lengthOfLongestSubstring(s);
