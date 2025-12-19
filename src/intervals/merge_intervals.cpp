90ed4a5e730ef79cb6c30aba837621777f1cf2d5
Eric Rostedt <rostedteric@gmail.com>
2025-12-19 13:55:22 +0100

    merge intervals

--- src/intervals/merge_intervals.cpp
+++ src/intervals/merge_intervals.cpp
@@ -23,7 +23,9 @@ intervals[i].length == 2
 0 <= starti <= endi <= 104
 */
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 #include "assert.hpp"
 
@@ -31,11 +33,46 @@ using namespace std;
 class Solution
 {
   public:
+    static bool interval_cmp(const vector<int> &i1, const vector<int> &i2)
+    {
+        return i1.front() < i2.front();
+    }
+
     vector<vector<int>> merge(vector<vector<int>> &intervals)
     {
-        NOT_IMPLEMENTED;
+        if (intervals.empty())
+        {
+            return {};
+        }
+
+        sort(begin(intervals), end(intervals), interval_cmp);
+        vector<vector<int>> merged{intervals.front()};
+        for (size_t i = 1; i < intervals.size(); ++i)
+        {
+            auto &last = merged.back();
+            auto &curr = intervals[i];
+
+            // Overlap
+            if (curr.front() <= last.back())
+            {
+                last.back() = max(last.back(), curr.back());
+            }
+            else
+            {
+                merged.push_back(curr);
+            }
+        }
+        return merged;
     }
 };
+
+void print_intervals(const vector<vector<int>> &intervals)
+{
+    for (const auto &i : intervals)
+    {
+        std::cout << i.front() << ", " << i.back() << std::endl;
+    }
+}
 
 int main()
 {
@@ -43,8 +80,11 @@ int main()
     {
         std::vector<std::vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
 
+        print_intervals(intervals);
         const std::vector<std::vector<int>> expected_output = {{1, 6}, {8, 10}, {15, 18}};
         const std::vector<std::vector<int>> output = solution.merge(intervals);
+        std::cout << "***************" << std::endl;
+        print_intervals(output);
 
         Assert::matrix_equals(output, expected_output);
     }
